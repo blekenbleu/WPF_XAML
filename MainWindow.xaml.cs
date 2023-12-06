713ed09c0ec2a4eba49089b18b4c094b7d748cf8
Steven Carmichael <blekenbleu@gmail.com>
2023-12-06 11:37:56 -0500

    add state 2 for captured mouse
    keep MessageBox on top

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -56,7 +56,7 @@ namespace WPF_XAML
 				// InputInterceptor.Initialize() absolutely must be run before new Intercept()!!
 				if (!InputInterceptor.Initialize())  // fails if DLL not linked
 				{
-					MessageBox.Show("Invalid or missing DLL;  closing", "InputInterceptor.Initialize()");
+					MessageBox.Show(Application.Current.MainWindow, "Invalid or missing DLL;  closing", "InputInterceptor.Initialize()");
 					state = 99;
 					Close();
 				}
@@ -65,13 +65,13 @@ namespace WPF_XAML
 
 				if (!Intermouse.Initialize(WriteStatus))
 				{
-					MessageBox.Show("No interception", "Intermouse.Initialize()");
+					MessageBox.Show(Application.Current.MainWindow, "No interception", "Intermouse.Initialize()");
 					state = 99;
 					Close();
 				}
 
 			} catch(Exception exception) {
-				MessageBox.Show("probably bad: '" 
+				MessageBox.Show(Application.Current.MainWindow, "probably bad: '" 
 				+ InputInterceptor.DPath + "'\n" + exception,
 				"InputInterceptor.Initialize() Exception"); 
 				Close();
@@ -95,22 +95,23 @@ namespace WPF_XAML
 				select.Content = "OK";
 				state = 99;
 			}
-			else if (1 != state)
+			else if (0 == state)
 			{
-				SHlabel.Content = $"mouse {Intercept.Stroke[0]} selected";
+				SHlabel.Content = $"state {state}:  mouse {Intercept.Stroke[0]} selected";
 				Intercept.Selected = Intercept.Stroke[0];
 
                 select.Content = "Click to deselect";
 				capture.Visibility = Visibility.Visible;
 				state = 1;
 			}
-			else
+			else	// deselect if state 1 or 2
 			{
 				Intercept.Selected = 0;
 				capture.Visibility = Visibility.Hidden;
 				if (1 < Intercept.devices.Count) {
-                	SHlabel.Content = "Left-click 'Select' using mouse to be captured for SimHub";
+                	SHlabel.Content = $"state {state}:  Left-click 'Select' using mouse to be captured for SimHub";
 					select.Content = "Select current device";
+					capture.Content = "Capture selected device for SimHub use";
 					state = 0;
 				}
 			}
@@ -118,14 +119,20 @@ namespace WPF_XAML
 
 		bool Hooked()		// what to do when a mouse is hooked; e.g. change callback
 		{
-			Intermouse?.Devices();	// iterate thru intercepted devices
+			Intermouse?.Devices(Intercept.Selected);	// if 0, iterate thru all predicate devices
 			this.Close();
 			return true;
 		}
 
 		private void Capture_Click(object sender, RoutedEventArgs e)
 		{
-			capture.Visibility = Visibility.Hidden;
+			if (2 == state)
+			{
+				Intercept.Stroke[1] = Intercept.Stroke[2] = Intercept.Stroke[3] = Intercept.Stroke[4] = 0;
+				return;
+			}
+			// capture.Visibility = Visibility.Hidden;
+			capture.Content = "click to center captured coordinates";
 			Hooked();
 		}
 
@@ -134,13 +141,15 @@ namespace WPF_XAML
 			if (99 != state)
 			{
 				// Handle interception unhooking
-				var result = MessageBox.Show("state: " + state
-						   + ((0 < state) ? $"; Unhook mouse {Intercept.Selected}?" : "; Done?"),
+				var result = MessageBox.Show(Application.Current.MainWindow, //"state: " + state + ";  " +
+											 ((0 < state) ? $"Unhook mouse {Intercept.Selected} and exit?" : "Done?"),
 											 "Closing",			   // messageBox caption
 											 MessageBoxButton.YesNo);
 
 				// User doesn't want to close, cancel closure
 				e.Cancel = (result == MessageBoxResult.No);
+				if (1 == state)
+					state++;	// different capture handling
 			}
 		}
 
